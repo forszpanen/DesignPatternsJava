f7390222250af9fd77563c63f96f25758135ea97
forszpanen <filipforszpaniak@gmail.com>
2017-02-07 00:35:51 +0100

    init commit

---
+++ src/main/java/DesignPatterns.java
@@ -0,0 +1,8 @@
+/**
+ * Created by filip on 07.02.2017.
+ */
+public class DesignPatterns {
+    public static void main(String[] args) {
+        System.out.println("Design Patterns in Java");
+    }
+}
